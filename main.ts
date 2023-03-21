3a9ea696d669f1d73ebdc637e9d8d06f1119dba3
yassoumnif <128477547+yassoumnif@users.noreply.github.com>
2023-03-21 12:05:06 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,39 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("42")
+    basic.showString("42")
+})
+input.onButtonPressed(Button.B, function () {
+    let valeur = 0
+    let _42 = 0
+    if (valeur == _42) {
+        basic.showString("B")
+    } else {
+        let _4F = 0
+        if (valeur == _4F) {
+            basic.showString("O")
+        } else {
+            let _4A = 0
+            if (valeur == _4A) {
+                basic.showString("N")
+            } else {
+                if (valeur == _4F) {
+                    basic.showString("J")
+                } else {
+                    if (valeur == _4F) {
+                        basic.showString("O")
+                    } else {
+                        let _55 = 0
+                        if (valeur == _55) {
+                            basic.showString("U")
+                        } else {
+                            let _52 = 0
+                            if (valeur == _52) {
+                                basic.showString("R")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+})
